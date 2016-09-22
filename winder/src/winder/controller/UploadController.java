b12045d69afac4bcb60248a7e0d4bb7e26a457e4
cs620 <cs620@DESKTOP-690SHN9>
2016-09-22 20:20:45 +0900

    0922 08:20

--- winder/src/winder/controller/UploadController.java
+++ winder/src/winder/controller/UploadController.java
@@ -1,11 +1,9 @@
 package winder.controller;
 
-import javax.activation.CommandMap;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import winder.service.UploadFileService;
@@ -23,10 +21,16 @@ public class UploadController {
 	
 	//파일 업로드
 	@RequestMapping(value="insertFile")
-	public ModelAndView insertFile(CommandMap commandMap, HttpServletRequest request) throws Exception{
-	    ModelAndView mv = new ModelAndView("redirect:/sample/openBoardList.do");
-	     
-	    uploadFileService.insertFile(commandMap.getMap(), request);
+	public ModelAndView insertFile(MultipartHttpServletRequest mRequest) throws Exception{
+	    
+		ModelAndView mv = new ModelAndView();
+		
+		if(uploadFileService.insertFile(mRequest)){
+			mv.addObject("result", "성공");
+		}else{
+			mv.addObject("result","실패");
+		}
+		mv.setViewName("mypage/upload");
 	     
 	    return mv;
 	}
