e7d20a49366a21532b35638f2b465b9774705d4f
cs620 <cs620@DESKTOP-690SHN9>
2016-10-06 20:52:13 +0900

    커밋

--- winder/src/winder/vo/UploadfileVO.java
+++ winder/src/winder/vo/UploadfileVO.java
@@ -1,37 +1,44 @@
 package winder.vo;
 
 public class UploadfileVO {
-	private int uno, pno;
-	private String upath, utitle, ucontent, udate, id;
-	public int getUno() {
-		return uno;
-	}
-	public void setUno(int uno) {
-		this.uno = uno;
-	}
-	public int getPno() {
-		return pno;
-	}
-	public void setPno(int pno) {
-		this.pno = pno;
-	}
-	public String getUpath() {
-		return upath;
-	}
-	public void setUpath(String upath) {
-		this.upath = upath;
-	}
+	private int uno, boardno;
+	private String utitle,originalname,storedname, usize,udate, id, delfile;
+	
 	public String getUtitle() {
 		return utitle;
 	}
 	public void setUtitle(String utitle) {
 		this.utitle = utitle;
 	}
-	public String getUcontent() {
-		return ucontent;
+	public int getUno() {
+		return uno;
 	}
-	public void setUcontent(String ucontent) {
-		this.ucontent = ucontent;
+	public void setUno(int uno) {
+		this.uno = uno;
+	}
+	public int getBoardno() {
+		return boardno;
+	}
+	public void setBoardno(int boardno) {
+		this.boardno = boardno;
+	}
+	public String getOriginalname() {
+		return originalname;
+	}
+	public void setOriginalname(String originalname) {
+		this.originalname = originalname;
+	}
+	public String getStoredname() {
+		return storedname;
+	}
+	public void setStoredname(String storedname) {
+		this.storedname = storedname;
+	}
+	public String getUsize() {
+		return usize;
+	}
+	public void setUsize(String usize) {
+		this.usize = usize;
 	}
 	public String getUdate() {
 		return udate;
@@ -45,7 +52,11 @@ public class UploadfileVO {
 	public void setId(String id) {
 		this.id = id;
 	}
+	public String getDelfile() {
+		return delfile;
+	}
+	public void setDelfile(String delfile) {
+		this.delfile = delfile;
+	}
 	
-	
-
 }
